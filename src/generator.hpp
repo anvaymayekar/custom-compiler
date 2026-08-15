3cf7863a4f2c8f0322ca6c4d8346298b6462b6e9
Anvay Mayekar <anvaay@gmail.com>
2026-08-15 20:15:56 +0530

    stack setup with expression parsing

--- src/generator.hpp
+++ src/generator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <utility>
 
 #include "parser.hpp"
@@ -15,9 +16,19 @@ class Generator {
             void operator()(const NodeExprAnk &exprAnk) {
                 _gen->_output << "    mov rax," << exprAnk.ank.value.value()
                               << "\n";
-                _gen->_output << "    push rax\n";
+                _gen->push("rax");
             }
             void operator()(const NodeExprId &exprId) {
+                auto temp = exprId.id.value.value();
+                if (!_gen->_vars.contains(temp)) {
+                    std::cerr << "Undeclared identifier: " << temp << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+                const auto &var = _gen->_vars.at(temp);
+                std::stringstream offset;
+                offset << "QWORD [rsp + "
+                       << (_gen->_stackSize - var.stackLoc) * 4 << "]\n";
+                _gen->push(offset.str());
             }
         };
         ExprVisitor visitor{._gen = this};
@@ -30,10 +41,19 @@ class Generator {
             void operator()(const NodeStmtShevat &stmtShevat) const {
                 _gen->genExpr(stmtShevat.expr);
                 _gen->_output << "    mov rax, 60\n";
-                _gen->_output << "    pop rdi,\n";
+                _gen->pop("rdi");
                 _gen->_output << "    syscall\n";
             }
             void operator()(const NodeStmtAnk &stmtAnk) {
+                auto temp = stmtAnk.id.value.value();
+                if (_gen->_vars.contains(temp)) {
+                    std::cerr << "Identifier already declared: " << temp
+                              << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+                _gen->_vars.insert({stmtAnk.id.value.value(),
+                                    Var{.stackLoc = _gen->_stackSize}});
+                _gen->genExpr(stmtAnk.expr);
             }
         };
         StmtVisitor visitor{._gen = this};
@@ -50,6 +70,21 @@ class Generator {
     }
 
    private:
+    void push(const std::string &reg) {
+        _output << "    push " << reg << "\n";
+        _stackSize++;
+    }
+    void pop(const std::string &reg) {
+        _output << "    pop " << reg << "\n";
+        _stackSize--;
+    }
+
+    struct Var {
+        size_t stackLoc;
+    };
+
     const NodeProg _prog;
     std::stringstream _output;
+    size_t _stackSize = 0;
+    std::unordered_map<std::string, Var> _vars{};
 };
